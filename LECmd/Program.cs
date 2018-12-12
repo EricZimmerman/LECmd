839e5f5ea0d12e160f4ef76b828fd40a5dcd31dd
Eric Zimmerman <saericzimmerman@gmail.com>
2018-12-12 10:17:47 -0500

    nlog path tweak

--- LECmd/Program.cs
+++ LECmd/Program.cs
@@ -1421,9 +1421,10 @@ namespace LnkCmd
             return vendor;
         }
 
+        private static readonly string BaseDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         private static void SetupNLog()
         {
-            if (File.Exists("Nlog.config"))
+            if (File.Exists( Path.Combine(BaseDirectory,"Nlog.config")))
             {
                 return;
             }
